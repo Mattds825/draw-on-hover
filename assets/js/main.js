c0a7687b7053f9fa0efdb5abd90cbcbba8301d99
Matthew Da Silva <matthewgdasilva25@gmail.com>
2024-08-13 09:29:08 +0100

    add spring

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,49 +1,52 @@
 const canvas = document.querySelector("canvas");
-const ctx = canvas.getContext('2d');
+const ctx = canvas.getContext("2d");
 
 const pointer = {
-    x: .5 * window.innerWidth,
-    y: .5 * window.innerHeight,
-}
+  x: 0.5 * window.innerWidth,
+  y: 0.5 * window.innerHeight,
+};
 
-window.addEventListener("click", e =>{
-    updateMousePosition (e.pageX, e.pageY);
+const params = {
+  spring: 0.4,
+};
+
+window.addEventListener("click", (e) => {
+  updateMousePosition(e.pageX, e.pageY);
 });
-window.addEventListener("mousemove", e =>{
-    updateMousePosition (e.pageX, e.pageY);
+window.addEventListener("mousemove", (e) => {
+  updateMousePosition(e.pageX, e.pageY);
 });
-window.addEventListener("touchmove", e =>{
-    updateMousePosition (e.targetTouches[0].pageX, e.targetTouches[0].pageY);
+window.addEventListener("touchmove", (e) => {
+  updateMousePosition(e.targetTouches[0].pageX, e.targetTouches[0].pageY);
 });
 
-window.addEventListener("resize", setupCanvas);//setup canvas if window resizes
+window.addEventListener("resize", setupCanvas); //setup canvas if window resizes
 
 function updateMousePosition(eX, eY) {
-    pointer.x = eX;
-    pointer.y = eY;
+  pointer.x = eX;
+  pointer.y = eY;
 }
 
 function setupCanvas() {
-    canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 }
 
-
-const p = {x: 0, y: 0}; // coordinate to draw
+const p = { x: 0, y: 0 }; // coordinate to draw
 
 setupCanvas();
 update(0);
 
 function update(t) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // copy cursor position
-    p.x = pointer.x;
-    p.y = pointer.y;
-    // draw a dot
-    ctx.beginPath();
-    ctx.arc(p.x, p.y, 5, 0, 2 * Math.PI);
-    ctx.fill();
+  // copy cursor position
+  p.x += (pointer.x - p.x) * params.spring;
+  p.y += (pointer.y - p.y) * params.spring;
+  // draw a dot
+  ctx.beginPath();
+  ctx.arc(p.x, p.y, 5, 0, 2 * Math.PI);
+  ctx.fill();
 
-    window.requestAnimationFrame(update);
+  window.requestAnimationFrame(update);
 }
